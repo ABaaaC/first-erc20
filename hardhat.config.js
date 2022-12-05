53a4a1111bcb68a33fbf6fcb15203a2ca5ea1f0d
Stanislav <rus.abaaac@gmail.com>
2022-12-05 21:08:44 +0600

    CHange hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,26 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+API_ALCHEMY = "" 
+PRIV_KEY = ""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  sources: "./contracts",
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/" + API_ALCHEMY,
+      accounts: [PRIV_KEY]
     }
-  }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
